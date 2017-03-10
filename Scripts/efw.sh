d71bb5cde93bd9c2a3857cffb79ac1344beb86aa
Dustin Franco <dustin@edyn.com>
2017-03-10 12:27:58 -0800

    added newlines

--- Scripts/efw.sh
+++ Scripts/efw.sh
@@ -2,3 +2,4 @@ if [$1 == "valve"]; then
 	Python /Users/dustinfranco/Edyn/firmware-valve/Scripts/efw_python.py "$*"
 else [$1 == "sensor"]; then 
 	Python /Users/dustinfranco/Edyn/firmware-sensor/Scripts/efw_python.py "$*"
+	
